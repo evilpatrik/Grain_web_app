ab464050c3bf39ea4fb0f0ed8d480c90535b6381
evilpatrik <aminkarashi8282@gmail.com>
2025-05-04 14:03:07 +0330

    Refactor app.py to register blueprints and update imports

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template
 from models import db, bcrypt, User
-from auth import auth
-
+from blueprints.auth import auth
+from blueprints.dashboard import dashboard
 app = Flask(__name__)
 
 # Configuration
-app.config['SECRET_KEY'] = 'your-secret-key-here'  # Change this to a secure secret key
+app.config['SECRET_KEY'] = 'amin-aras-mehran'  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///grains.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Initialize extensions
+
 db.init_app(app)
 bcrypt.init_app(app)
 
-# Register blueprints
+
 app.register_blueprint(auth)
+app.register_blueprint(dashboard)
 
 # Create database tables and default admin
 with app.app_context():
@@ -27,10 +28,10 @@ with app.app_context():
         db.session.add(admin)
         db.session.commit()
 
-# Home route
+# Login route
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
