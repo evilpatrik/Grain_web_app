b1dc6362909f1895f038fa249cebaf6c6fdb0f3c
evilpatrik <aminkarashi8282@gmail.com>
2025-05-15 11:10:33 +0330

    add update employee information route

--- blueprints/dashboard/manager_routes.py
+++ blueprints/dashboard/manager_routes.py
@@ -57,3 +57,35 @@ def delete_employee(employee_id):
         UserCRUD.delete_user(employee_id)
         return jsonify({'success': True, 'deleted_id': employee_id})
     return jsonify({'error': 'کارمند یافت نشد'}), 404
+
+@dashboard.route('/api/manager/employees/<int:employee_id>', methods=['PUT'])
+@login_required
+@role_required('manager')
+def update_employee(employee_id):
+    data = request.get_json()
+    user = UserCRUD.get_user_by_id(employee_id)
+    if not user or user.role != 'employee':
+        return jsonify({'error': 'کارمند یافت نشد'}), 404
+    
+    name = data.get('name')
+    family = data.get('family')
+    phone = data.get('phone')
+    password = data.get('password')
+    
+    if UserCRUD.get_user_by_phone(phone):
+        return jsonify({'error': 'Phone number already exists'}), 400
+
+
+    user.name = name
+    user.family = family
+    user.phone = phone
+    if password:
+        if user.check_password(password):
+            return jsonify({'error': 'رمز جدیدی وارد کنید'}), 400
+        user.set_password(password)
+    
+
+    db.session.commit()
+    return jsonify({'message': 'بروزرسانی با موفقیت انجام شد'}),201
+
+
