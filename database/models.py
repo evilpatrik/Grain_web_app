b05b0426395fc9fbc28f7658207c029eff23f37c
unknown <mehrankarimii1380@gmail.com>
2025-05-15 22:01:47 +0330

    some bugs fixed in User class two columns do not need to be unique :name and family name

--- database/models.py
+++ database/models.py
@@ -7,8 +7,8 @@ bcrypt = Bcrypt()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    family = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    family = db.Column(db.String(80), nullable=False)
     phone = db.Column(db.String(80), unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
