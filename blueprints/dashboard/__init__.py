559bb0ebfba85f3218dd0634309872188b3d24d0
evilpatrik <aminkarashi8282@gmail.com>
2025-05-16 17:29:56 +0330

    add employee_route to __init__.py

--- blueprints/dashboard/__init__.py
+++ blueprints/dashboard/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 dashboard = Blueprint('dashboard', __name__)
 
-from . import routes,manager_routes,admin_routes
+from . import routes,manager_routes,admin_routes,employee_routes
