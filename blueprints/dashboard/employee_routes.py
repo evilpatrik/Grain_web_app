5184c2158c01b1d49f0857a50fd37a3349f1da6a
evilpatrik <aminkarashi8282@gmail.com>
2025-05-16 17:33:29 +0330

    Add buy route to get the id of an product and buy from a user -->update price and quantity

--- blueprints/dashboard/employee_routes.py
+++ blueprints/dashboard/employee_routes.py
@@ -18,3 +18,24 @@ def get_employee_products():
     products = ProductCRUD.get_all_products()
     return jsonify([product.to_dict() for product in products])
 
+@dashboard.route('/api/employee/products/<int:product_id>/buy', methods=['POST'])
+def buy_product(product_id):
+    data = request.get_json()
+    quantity = data.get('quantity')
+    price = data.get('price')
+
+    product = ProductCRUD.get_product_by_id(product_id)
+
+    new_order = Order(
+        name=product.name,
+        quantity=quantity,
+        price=price,
+        types='buy'
+    )
+    new_order.total_price(price,quantity)
+
+    db.session.add(new_order)
+    product.price = price 
+    db.session.commit()
+
+    return jsonify({'message': 'سفارش با موفقیت ثبت شد'}), 201
